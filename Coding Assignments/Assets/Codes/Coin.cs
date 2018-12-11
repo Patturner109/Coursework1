73c339499989869f093998782e88f31c7cb40d77
Paul Sinnett <paul.sinnett@gmail.com>
2018-12-11 15:28:17 +0000

    Initial commit

---
+++ Coding Assignments/Assets/Codes/Coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 90 * Time.deltaTime, 0, 0);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<PlayerController>().points++;
+            //Add 1 to points.
+            Destroy(gameObject); //Destroy Objects
+        }
+    }
+}
---
+++ Coding Assignments/Assets/Codes/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 18;
+    public int points = 0;
+    private Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+        rig.MovePosition(transform.position + movement);
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Score : " + points);
+    }
+}
